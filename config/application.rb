e29e82da061b36efbf309d54181a16a6c58f1dc0
shotaro yamamoto <notawhisper56@gmail.com>
2020-10-14 17:55:00 +0900

    timezoneを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Popinion
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
