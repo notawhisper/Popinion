6495bcd3784cd5b39dd993e135a038589cf31d77
shotaro yamamoto <notawhisper56@gmail.com>
2020-10-14 17:49:57 +0900

    rails g 実行時に不要ファイルが作成されないよう設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module Popinion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
