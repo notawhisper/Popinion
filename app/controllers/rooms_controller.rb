d0512eb21c24fbee36104bbdffe2dc20d0c56d13
shotaro yamamoto <notawhisper56@gmail.com>
2020-10-16 22:54:57 +0900

    [add]roomsコントローラを追加

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,23 @@
+class RoomsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  # private
+  # def set_room
+  #
+  # end
+  #
+  # def room_params
+  #   params.require(:room).permit(:name, :text, :group_id, :distinguish_speaker,
+  #                                :let_guests_view_all, :show_member_list)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/new'
+  get 'rooms/edit'
+  get 'rooms/show'
   root 'top#index'
   devise_for :users
   resources :users, only: [:show]
