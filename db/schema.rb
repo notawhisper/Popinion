67b58d01392e14f4b371819365b3d5d1a8866cf7
shotaro yamamoto <notawhisper56@gmail.com>
2020-10-16 23:04:18 +0900

    [add]roomsテーブルにhost_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_143247) do
+ActiveRecord::Schema.define(version: 2020_10_16_135549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_143247) do
     t.boolean "show_member_list", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "host_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_10_15_143247) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "users", column: "host_id"
 end
