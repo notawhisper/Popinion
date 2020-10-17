972553a4e4b6d7c06ac1328f1c72875ed69330b1
shotaro yamamoto <notawhisper56@gmail.com>
2020-10-17 12:33:29 +0900

    [add]簡単なチャットルームCRUD機能を作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,23 +1,51 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit, :update, :show, :destroy]
+  before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @room = current_user.host_rooms.build
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to @room, notice: t('.success')
+    else
+      render :new, notice: t('.failed')
+    end
   end
 
   def edit
   end
 
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: t('.success')
+    else
+      render :edit, notice: t('.failed')
+    end
+  end
+
   def show
   end
 
-  # private
-  # def set_room
-  #
-  # end
-  #
-  # def room_params
-  #   params.require(:room).permit(:name, :text, :group_id, :distinguish_speaker,
-  #                                :let_guests_view_all, :show_member_list)
-  # end
+  def destroy
+    if @room.destroy
+      redirect_to rooms_path, notice: t('.success')
+    end
+  end
+
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :description, :group_id, :distinguish_speaker,
+                                 :let_guests_view_all, :show_member_list, :host_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'rooms/new'
-  get 'rooms/edit'
-  get 'rooms/show'
-  root 'top#index'
   devise_for :users
+  root "top#index"
   resources :users, only: [:show]
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
