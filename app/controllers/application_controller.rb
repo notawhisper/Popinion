f73b2fb51c0bc46750b313bb2efff2480585ae21
shotaro yamamoto <notawhisper56@gmail.com>
2020-10-24 22:09:41 +0900

    [fix]devise関連のviewを調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  def after_sign_in_path_for(resource)
-    user_path(id: current_user.id)
-  end
-
   def reject_request_from_guest
     if @room && (current_user != @room.host)
       redirect_to @room, notice: "権限がありません"
